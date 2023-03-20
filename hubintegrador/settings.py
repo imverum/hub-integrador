2338c9e171d7c5c6e62ee6d3f1efa18e171727d8
Luiz Arthur <58349638+larthurfs@users.noreply.github.com>
2023-03-20 12:05:31 -0300

    hub-integrador-production

--- hubintegrador/settings.py
+++ hubintegrador/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://hub-integrador-production.up.railway.app/']
 
 
 # Application definition
